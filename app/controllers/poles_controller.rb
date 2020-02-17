32dfe90b44c9ce0b24931e6a1e99a602445dadc2
mikael autheman <mika@atoomik.fr>
2020-02-17 15:01:12 +0100

    pole policy

--- app/controllers/poles_controller.rb
+++ app/controllers/poles_controller.rb
@@ -1,15 +1,27 @@
 class PolesController < ApplicationController
-  before_action :set_pole, only: [:show]
+  before_action :set_pole, only: %i[show edit update]
   def index
-    @poles = Pole.all
+    @poles = policy_scope(Pole)
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @pole.update(pole_params)
   end
 
   private
 
   def set_pole
     @pole = Pole.find(params[:id])
+    authorize @pole
+  end
+
+  def pole_params
+    params.require(:pole).permit(:name, :adresse, :adresse2, :code_postal, :ville, :bulle, :tel, :description, :principes, :email)
   end
 end
