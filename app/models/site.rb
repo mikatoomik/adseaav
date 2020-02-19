5d92f635e08e79e1c83dbb20d85790b3c3f90476
mikael autheman <mika@atoomik.fr>
2020-02-19 15:39:08 +0100

    seed antennes

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,9 +1,9 @@
 class Site < ApplicationRecord
   has_many :antennes, dependent: :destroy
   def geoaddress
-    [adresse, adresse2, code_postal, ville].compact.join(', ')
+    [adresse, code_postal, ville].compact.join(', ')
   end
   geocoded_by :geoaddress
-  after_validation :geocode
+  after_validation :geocode, if: :will_save_change_to_adresse?
   has_one_attached :photo
 end
