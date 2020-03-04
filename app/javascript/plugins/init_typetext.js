55ccd84b77e43f41219af626190b888f4557fa5f
mikael autheman <mika@atoomik.fr>
2020-03-04 10:01:49 +0100

    image back

--- app/javascript/plugins/init_typetext.js
+++ app/javascript/plugins/init_typetext.js
@@ -4,11 +4,7 @@ const typedtext = document.getElementById('typed-text');
 const initTypetext = () => {
   if (typedtext) {
     new Typed('#typed-text', {
-      strings: ["Association Départementale",
-        "de Sauvegarde de l’Enfance,",
-        "de l’Adolescence",
-        "et des Adultes en difficulté",
-        "du Var"],
+      strings: ["<strong>A</strong>ssociation <strong>D</strong>épartementale de <strong>S</strong>auvegarde de l’<strong>E</strong>nfance de l’<strong>A</strong>dolescence et des <strong>A</strong>dultes en difficulté du <strong>V</strong>ar"],
       typeSpeed: 100,
       loop: true
     });
