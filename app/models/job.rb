d580646eb1b30fb58e76cf8267e003e2fb459838
mikael autheman <mika@atoomik.fr>
2020-03-14 23:05:10 +0100

    validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,15 @@ class Job < ApplicationRecord
   belongs_to :service
   belongs_to :site
 
+  validates :intitule, presence: true
+  validates :date_debut, presence: true
+  validates :date_fin, presence: true, unless: :CDI?
+  validates :n_pole_emploi, uniqueness: true
+
+  def CDI?
+    contrat == 'CDI'
+  end
+
   include PgSearch::Model
   pg_search_scope :search_in_jobs,
     against: [ :contrat, :etp, :intitule ],
